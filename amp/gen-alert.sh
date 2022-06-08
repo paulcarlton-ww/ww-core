f187a961072c45c9e2d5b0eb6df109d7fab6134a
paulcarlton-ww <paul.carlton@weave.works>
2022-06-08 15:43:01 +0100

    deploy prometheus

--- amp/gen-alert.sh
+++ amp/gen-alert.sh
@@ -1,5 +1,8 @@
-#!/bin/bash/env
-
+#!/bin/bash
+set -x
 kubectl port-forward -n webapp svc/podinfo 9898 &
 port_forward=$!
-
+sleep 2
+curl -s http://127.0.0.1:9898/panic
+sleep 2
+kill -9 $port_forward
